9ae07f5bb8b60ad2cdc5154249a87f986561e1e2
DrMatters <qseminq@gmail.com>
2018-10-02 11:21:21 +0300

    clear

---
+++ network/main.js
@@ -0,0 +1,70 @@
+$(document).ready(() => {
+
+    const container = document.getElementById('mynetwork');
+    const data = {
+        nodes: nodes,
+        edges: edges
+    };
+    const options = {
+        physics: {
+            stabilization: false,
+            barnesHut: {
+                gravitationalConstant: -80000,
+                springConstant: 0.001,
+                springLength: 200
+            }
+        },
+
+        nodes: {
+            shape: 'dot',
+            scaling: {
+
+                label: {
+                    min: 40,
+                    max: 300
+                },
+
+                min: 20,
+                max: 440
+            },
+            font: {
+                size: 12,
+                face: 'Tahoma'
+            },
+        },
+        edges: {
+            scaling: {
+                min: 5,
+                max: 100
+            },
+            color: {
+                inherit: 'from'
+            },
+            smooth: {
+                type: 'continuous'
+            }
+        },
+        interaction: {
+            tooltipDelay: 200,
+            hideEdgesOnDrag: true
+        },
+        layout: {
+            improvedLayout: false
+        }
+    };
+
+    const network = new vis.Network(container, data, options);
+
+    $('.js-example-basic-single').select2({
+        data: nodes.map(node => ({
+            id: node.id,
+            text: node.label
+        }))
+    });
+
+    $('.js-example-basic-single').on('select2:select', e => {
+        network.focus(e.params.data.id, {
+            scale: 0.25,
+        });
+    });
+});
