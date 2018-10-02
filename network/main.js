6b3e77e62e6d3532565e05299051d0d3472e381c
DrMatters <qseminq@gmail.com>
2018-10-02 11:39:38 +0300

    fix

--- network/main.js
+++ network/main.js
@@ -1,70 +1,68 @@
-$(document).ready(() => {
 
-    const container = document.getElementById('mynetwork');
-    const data = {
-        nodes: nodes,
-        edges: edges
-    };
-    const options = {
-        physics: {
-            stabilization: false,
-            barnesHut: {
-                gravitationalConstant: -80000,
-                springConstant: 0.001,
-                springLength: 200
-            }
-        },
-
-        nodes: {
-            shape: 'dot',
-            scaling: {
-
-                label: {
-                    min: 40,
-                    max: 300
-                },
-
-                min: 20,
-                max: 440
-            },
-            font: {
-                size: 12,
-                face: 'Tahoma'
-            },
-        },
-        edges: {
-            scaling: {
-                min: 5,
-                max: 100
-            },
-            color: {
-                inherit: 'from'
-            },
-            smooth: {
-                type: 'continuous'
-            }
-        },
-        interaction: {
-            tooltipDelay: 200,
-            hideEdgesOnDrag: true
-        },
-        layout: {
-            improvedLayout: false
+const container = document.getElementById('mynetwork');
+const data = {
+    nodes: nodes,
+    edges: edges
+};
+const options = {
+    physics: {
+        stabilization: false,
+        barnesHut: {
+            gravitationalConstant: -80000,
+            springConstant: 0.001,
+            springLength: 200
         }
-    };
+    },
 
-    const network = new vis.Network(container, data, options);
+    nodes: {
+        shape: 'dot',
+        scaling: {
 
-    $('.js-example-basic-single').select2({
-        data: nodes.map(node => ({
-            id: node.id,
-            text: node.label
-        }))
-    });
+            label: {
+                min: 40,
+                max: 300
+            },
 
-    $('.js-example-basic-single').on('select2:select', e => {
-        network.focus(e.params.data.id, {
-            scale: 0.25,
-        });
+            min: 20,
+            max: 440
+        },
+        font: {
+            size: 12,
+            face: 'Tahoma'
+        },
+    },
+    edges: {
+        scaling: {
+            min: 5,
+            max: 100
+        },
+        color: {
+            inherit: 'from'
+        },
+        smooth: {
+            type: 'continuous'
+        }
+    },
+    interaction: {
+        tooltipDelay: 200,
+        hideEdgesOnDrag: true
+    },
+    layout: {
+        improvedLayout: false
+    }
+};
+
+document.network = new vis.Network(container, data, options);
+
+$('.js-example-basic-single').select2({
+    data: nodes.map(node => ({
+        id: node.id,
+        text: node.label
+    }))
+});
+
+$('.js-example-basic-single').on('select2:select', e => {
+    network.focus(e.params.data.id, {
+        scale: 0.25,
     });
 });
